8221e153ce58b66476b2dbef037ae69ab7e3486b
VereshaginDaniil <139614656+VereshaginDaniil@users.noreply.github.com>
2024-12-05 21:01:05 +0300

    Update models.py

--- phones/models.py
+++ phones/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=50)
     price = models.IntegerField()
